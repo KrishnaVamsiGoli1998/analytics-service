83d8133794d7f6b33442cb1a79a47072df56f8e4
Krishna Vamsi <krishnavamsigoli@gmail.com>
2025-05-11 14:46:13 +0530

    final commit

--- docker-image/import.ps1
+++
@@ -1,4 +0,0 @@
-# Import script for Analytics Service
-Write-Host "Importing Analytics Service Docker image..."
-docker load -i analytics-service.tar
-Write-Host "Analytics Service image imported successfully!"
